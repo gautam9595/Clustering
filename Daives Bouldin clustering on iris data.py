9968c53a20d6346b17285f40a461b7d7fcb57dd7
gautam9595 <47922035+gautam9595@users.noreply.github.com>
2019-03-26 02:51:13 +0530

    Update Daives Bouldin clustering on iris data.py

--- Daives Bouldin clustering on iris data.py
+++ Daives Bouldin clustering on iris data.py
@@ -1,12 +1,16 @@
+import numpy as np
 import pandas as pd
+import math
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
 from sklearn.datasets import load_iris
 from sklearn.metrics import davies_bouldin_score
 
-#Loading Dataset
 iris=load_iris()
-data=pd.DataFrame(iris.data,columns=iris.feature_names)
-
-#Building Model
-model=davies_bouldin_score(iris.data,iris.target)
-
-print('The Daives Bouldin score is ',model)
+score=[]
+for i in range(2,int(math.sqrt(len(iris.data)))):
+    label=KMeans(n_clusters=i).fit_predict(iris.data)
+    score.append(davies_bouldin_score(iris.data,label))
+    
+print('The Score is ',score)    
+plt.plot(range(2,int(math.sqrt(len(iris.data)))),score)
